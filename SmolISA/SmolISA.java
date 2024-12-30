631e3e2f028b4456d50c0a7e43f66ab6656b302b
zaim <zaimhibban@gmail.com>
2024-12-30 16:51:24 +0700

    Update comment

--- SmolISA/SmolISA.java
+++ SmolISA/SmolISA.java
@@ -1,6 +1,12 @@
 
 public class SmolISA{
     // The idea is to make the smallest ISA possible, essentially a VM
+    // Control
+    // Logic/Arithmetic
+    // Data movement
+
+    // End goal: Graphics, Network, Interactivity. Multiplayer ping pong.
+
     public static void main(String[] args) {
         System.out.println("bruh");
     }
