e83ccd6bdb5af36c98efcdea43b131e178b4611b
zaim <zaimhibban@gmail.com>
2024-12-30 16:44:35 +0700

    smolISA LFG

---
+++ SmolISA/SmolISA.java
@@ -0,0 +1,7 @@
+
+public class SmolISA{
+    // The idea is to make the smallest ISA possible, essentially a VM
+    public static void main(String[] args) {
+        System.out.println("bruh");
+    }
+}
